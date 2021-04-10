3722793ad5b0da30cc48d66536bf28c5f45a4d44
Mackenzie Snider <macksnider@gmail.com>
2021-04-10 14:07:07 -0700

    basic files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('img')
+let mack = document.querySelector('#mack')
+let aaron = document.querySelector('#aaron')
+
+mack.addEventListener('click', () => {
+	img.src = 'images/mack.png';
+})
+
+aaron.addEventListener('click', () => {
+	img.src = 'images/guyStock.png';
+})
